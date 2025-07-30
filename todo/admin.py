5b928961be5068c0fce96efedbbe020f3e78b97b
v1nrate <r.g2018@mail.ru>
2025-07-30 20:52:39 +0300

    база данных, создание админки

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from todo.models import TodoModel
+
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'term', 'priority', 'status')
+    list_filter = ('priority', 'status')
+    search_fields = ('title',)
+    list_editable = ('priority', 'status')
+    
+    
+admin.site.register(TodoModel, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoModel(models.Model):
+    STATUS_DICT = [
+        ('new', 'Новый'),
+        ('in_progress', 'В исполнении'),
+        ('done', 'Выполнено')
+    ]
+    title = models.CharField(verbose_name='Задача')
+    term = models.DateTimeField(null=True, blank=True, verbose_name='Срок')
+    creating_date = models.DateTimeField(verbose_name='Создана', auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Отредактировно')
+    priority = models.IntegerField(verbose_name='Приоритет')
+    status = models.CharField(choices=STATUS_DICT, verbose_name='Статус')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
